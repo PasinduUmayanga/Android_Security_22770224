c8c5962bf8d3cff64b77867d5d71b5913677eaa4
Pasindu Umayanga <pasinduumayanga93@gmail.com>
2023-10-27 23:07:12 +0530

    initial checkin

---
+++ app/src/main/java/com/example/android_security_22770224/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android_security_22770224;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username,password,repassword;
+    Button signUp,SignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+
+    }
+}
